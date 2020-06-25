7595f491f3fcb23451c7c519e548358c52722af7
DoanDong2404 <55686554+DoanDong123@users.noreply.github.com>
2020-06-25 19:18:26 +0700

    Edit Project

--- LTCSDL.DAL/NorthwindContext.cs
+++ LTCSDL.DAL/NorthwindContext.cs
@@ -52,7 +52,7 @@ namespace LTCSDL.DAL
             if (!optionsBuilder.IsConfigured)
             {
                 //To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Data Source=DAT\\MSSQLSERVER2019;Initial Catalog=Northwind;Integrated Security=True;");
+                optionsBuilder.UseSqlServer("Data Source=.\\SQLEXPRESS01;Initial Catalog=Northwind;Integrated Security=True");
             }
         }
 
